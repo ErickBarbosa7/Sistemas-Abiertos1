a865fff065222a412013bc15fef7bc3f211a5967
ErickBarbosa7 <l.bace040211@itses.edu.mx>
2025-10-06 22:02:17 -0600

    LinkedSignal

--- src/app/components/character-add/character-add.component.ts
+++ src/app/components/character-add/character-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, signal } from '@angular/core';
+import { Component, effect, EventEmitter, linkedSignal, Output, signal } from '@angular/core';
 import { Character } from '../../interface';
 
 @Component({
@@ -8,12 +8,46 @@ import { Character } from '../../interface';
 })
 export class CharacterAddComponent {
 
-  name = signal<string>('');
-  power = signal<number>(0);
-
+  
+  
   @Output() onNewCharacter = new EventEmitter<Character>();
+  //name = signal<string>('');
+  //power = signal<number>(0);
+  private nameSignal = signal<string>('');
+  private powerSignal = signal<number>(0);
 
-  addCharacter() {
+  name = linkedSignal(() => this.nameSignal());
+  power = linkedSignal(() => this.powerSignal());
+  constructor() {
+    // Efectos para sincronizar con LocalStorage
+    const nameStored = localStorage.getItem('character_name');
+    const powerStored = localStorage.getItem('character_power');
+
+    if (nameStored) this.nameSignal.set(JSON.parse(nameStored));
+    if (powerStored) this.powerSignal.set(JSON.parse(powerStored));
+
+    // Persistir cuando cambian
+    effect(() => {
+      localStorage.setItem('character_name', JSON.stringify(this.name()));
+      localStorage.setItem('character_power', JSON.stringify(this.power()));
+    });
+  }
+
+   addCharacter() {
+    if (!this.name().trim() || this.power() <= 0) return;
+
+    const newCharacter: Character = {
+      id: Date.now(),
+      name: this.name(),
+      power: this.power()
+    };
+
+    this.onNewCharacter.emit(newCharacter);
+    this.nameSignal.set('');
+    this.powerSignal.set(0);
+  }
+
+  /*addCharacter() {
     if (!this.name().trim() || this.power() <= 0) return;
 
     const newCharacter: Character = {
@@ -26,4 +60,8 @@ export class CharacterAddComponent {
     this.name.set('');
     this.power.set(0);
   }
+  
+  */
+ 
+ 
 }
